554cb007365b4cfd95bfad25ef6ee295abf3e8b8
vinicius aquino oliveira <viniusername@gmail.com>
2025-03-09 15:30:35 -0300

    Resolvido o exercicio

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("O que é, o que é...");
+Thread.Sleep(1000);
+Console.WriteLine();
+Console.Write("...O que o 4 disse para o 40?");
+Thread.Sleep(6000);
+Console.WriteLine();
+Console.WriteLine();
+Console.Write("Passa a bola.");
+Thread.Sleep(2000);
+Console.WriteLine();
+Console.WriteLine();
+Console.Write("Etec Adolpho Berezin");
+Console.WriteLine();
+Console.Write(@"
++-----------+
+| Vinicius  |
++-----------+");
+
